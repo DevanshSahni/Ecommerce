cf832cf8c7fd6c8ee1a46333faa9762a8dd82dbb
Devansh <devanshsahni@gmail.com>
2023-12-10 10:39:56 +0530

    Create signup page

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import waves from "../Assets/waves.png";
+import "../CSS/signup.css";
+import { Link } from "react-router-dom";
+import painter from "../Assets/Artist.png"
 
 const Signup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Signup
+  return (
+    <div className="signup">
+      <img className="signupWaves" src={waves} alt="" />
+      <h1 className="signupHeading">Signup</h1>
+      <form className="signupForm">
+      <img className="signupArtist" src={painter} alt="" />
+        <input
+          className="signupFormInput"
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          className="signupFormInput"
+          type="number"
+          placeholder="Number"
+          onChange={(e) =>
+            e.target.value.length <= 10 ? setNumber(e.target.value) : null
+          }
+          value={number}
+          maxLength={10}
+        />
+        <input
+          className="signupFormInput"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          className="signupFormInput"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <div className="signupFormFooter">
+          <button type="submit" className="signupFormButton">
+            Signup
+          </button>
+          <Link to={"/login"}>
+            <button type="button" className="signupFormLoginButton">
+              Login
+            </button>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
